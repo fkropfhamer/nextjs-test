44ffddadec1822748901cc044054f034b6c505e2
fkropfhamer <fabian.kropfhamer@sblum.de>
2020-10-21 13:26:03 +0200

    .

---
+++ components/layout.js
@@ -0,0 +1,10 @@
+import Footer from './footer';
+import Header from './header';
+
+export default function Layout(props) {
+    return <div>
+        <Header />
+        {props.children}
+        <Footer />
+    </div>
+}
